ec96e510428865c3d7c38f54cae4069992b2d26c
Daniel-Kharrat <danielkharrat2@gmail.com>
2025-05-15 21:22:46 +0300

    Update Daniel_Paste items-tracks.lua

--- Scripts/Daniel_Paste items-tracks.lua
+++ Scripts/Daniel_Paste items-tracks.lua
@@ -3,11 +3,26 @@ reaper.PreventUIRefresh(1)
 --Get cursor position
 local cursor_pos = reaper.GetCursorPosition()
 
+--Get time selection
+local start_time, end_time = reaper.GetSet_LoopTimeRange(false,false,0,0,false)
+
 --Get arrange view position
 local view_start, view_end = reaper.GetSet_ArrangeView2(0, false, 0, 0)
 
---Item: Paste items/tracks
-reaper.Main_OnCommand(42398,0)
+if start_time ~= end_time then
+  
+  --Move edit cursor to start of time selection
+  reaper.SetEditCurPos(start_time, false, false)
+
+  --Item: Paste items/tracks
+  reaper.Main_OnCommand(42398,0)
+
+else
+  
+  --Item: Paste items/tracks
+  reaper.Main_OnCommand(42398,0)
+
+end
 
 --Restore cursor position
 reaper.SetEditCurPos(cursor_pos, false, false)
@@ -15,9 +30,8 @@ reaper.SetEditCurPos(cursor_pos, false, false)
 --Restore arrange view position
 reaper.GetSet_ArrangeView2(0, true, 0, 0, view_start, view_end)
 
---SWS: Set time selection to selected items (skip if time selection exists)
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SAFETIMESEL"), 0)
-
+--Time selection: Set time selection to items
+reaper.Main_OnCommand(40290,0)
 
 -- Update the arrange view
 reaper.PreventUIRefresh(-1)

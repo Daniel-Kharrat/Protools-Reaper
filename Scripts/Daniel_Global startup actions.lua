b0eca1a99a29a18f87f85a60fc48f74feaabd62a
Daniel-Kharrat <danielkharrat2@gmail.com>
2025-05-15 15:48:23 +0300

    Update Daniel_Global startup actions.lua

--- Scripts/Daniel_Global startup actions.lua
+++ Scripts/Daniel_Global startup actions.lua
@@ -1,10 +1,10 @@
 -- Master script to run multiple actions at startup
 
 --Check for armed tracks
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS316a89c3c279b2fc864b69a9d4445aaf32444a6a"), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_RSe07de25faddbad1a82edccb0cf675ab4014f4a5a"), 0)
 --Check for muted tracks
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS64a0c50d779925fa7bd9a2c794273ad512b78e23"), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS089967a15b5ae97c96687243d33f3cfb3cce8071"), 0)
 --Check for soloed tracks
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS12cfa74a9cb866735aed4af132544a9a111a5f7b"), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS73872e52305a191a83f55f1811ac45c380e98ff5"), 0)
 
 

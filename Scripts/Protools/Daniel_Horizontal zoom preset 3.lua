1114ecf533ec8a876e6147d75c7edaed8f5b4735
Daniel-Kharrat <danielkharrat2@gmail.com>
2025-07-03 16:27:51 +0300

    Update Daniel_Horizontal zoom preset 3.lua

--- Scripts/Protools/Daniel_Horizontal zoom preset 3.lua
+++ Scripts/Protools/Daniel_Horizontal zoom preset 3.lua
@@ -11,6 +11,10 @@ local new_end = view_center + (target_range / 2)
 -- Set arrange new start and end
 reaper.PreventUIRefresh(1)
 reaper.GetSet_ArrangeView2(0, true, 0, 0, new_start, new_end)
+
+--SWS: Horizontal scroll to put edit cursor at 50%
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_HSCROLL50"),0)
+
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
 

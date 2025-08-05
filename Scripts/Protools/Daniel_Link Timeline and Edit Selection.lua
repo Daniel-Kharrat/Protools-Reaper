4e8194fdaa04f6f5791f2b50d267a46d40327e0d
Daniel-Kharrat <danielkharrat2@gmail.com>
2025-08-05 19:37:35 +0300

    Add files via upload

---
+++ Scripts/Protools/Daniel_Link Timeline and Edit Selection.lua
@@ -0,0 +1,17 @@
+reaper.set_action_options(1)
+
+local command_id = reaper.NamedCommandLookup("_RSdd4ba6262e05c57604ab621179a4552cf2bad49b")
+local state = reaper.GetToggleCommandState(command_id)
+
+function update_toolbar_button()
+
+    if state == 1 then
+        reaper.SetToggleCommandState(0, command_id, 0)
+    else
+        reaper.SetToggleCommandState(0, command_id, 1)
+    end
+
+    reaper.RefreshToolbar2(0, command_id)
+end
+
+update_toolbar_button()

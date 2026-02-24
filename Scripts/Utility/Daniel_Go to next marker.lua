0cded5b989e4c4d2c6756c46d42bef80a8dd6d49
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-02-24 23:51:59 +0200

    Add files via upload

---
+++ Scripts/Utility/Daniel_Go to next marker.lua
@@ -0,0 +1,8 @@
+--Time selection: Remove (unselect) time selection and loop points
+reaper.Main_OnCommand(40020,0)
+
+--Razor edit: Clear all areas
+reaper.Main_OnCommand(42406,0)
+
+--Markers: Go to next marker/project end
+reaper.Main_OnCommand(40173,0)
---
+++ Scripts/Utility/Daniel_Go to previous marker.lua
@@ -0,0 +1,8 @@
+--Time selection: Remove (unselect) time selection and loop points
+reaper.Main_OnCommand(40020,0)
+
+--Razor edit: Clear all areas
+reaper.Main_OnCommand(42406,0)
+
+--Markers: Go to previous marker/project start
+reaper.Main_OnCommand(40172,0)

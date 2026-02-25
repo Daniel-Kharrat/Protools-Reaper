341e98f416c8343bee7e5e7dfe41334f8438df96
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-02-25 15:28:37 +0200

    Update Daniel_Go to previous marker.lua

--- Scripts/Utility/Daniel_Go to previous marker.lua
+++ Scripts/Utility/Daniel_Go to previous marker.lua
@@ -4,5 +4,8 @@ reaper.Main_OnCommand(40020,0)
 --Razor edit: Clear all areas
 reaper.Main_OnCommand(42406,0)
 
+--Item: Unselect (clear selection of) all items
+reaper.Main_OnCommand(40289,0)
+
 --Markers: Go to previous marker/project start
 reaper.Main_OnCommand(40172,0)

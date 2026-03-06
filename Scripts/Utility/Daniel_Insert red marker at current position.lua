95082e20405978231f445cb8398c52b772e21223
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-03-06 22:10:47 +0200

    Update Daniel_Insert red marker at current position.lua

--- Scripts/Utility/Daniel_Insert red marker at current position.lua
+++ Scripts/Utility/Daniel_Insert red marker at current position.lua
@@ -1,33 +1,17 @@
---Markers: Insert marker at current position
-reaper.Main_OnCommand(40157, 0)
+reaper.Undo_BeginBlock()
 
-local max_idx = -1
-local enum_index_of_max = nil
-local max_pos, max_rgnend, max_name
+local playstate = reaper.GetPlayState()
+local cursor_pos
 
---Count markers and regions
-local _, num_markers, num_regions = reaper.CountProjectMarkers(proj)
-local total = num_markers + num_regions
-
---Find marker with highest ID
-for i=0, total-1 do
-  local retval, isrgn, pos, rgnend, name, idx = reaper.EnumProjectMarkers3(proj, i)
-
-  if retval and not isrgn then
-    if idx > max_idx then
-      max_idx = idx
-      enum_index_of_max = i
-      max_pos = pos
-      max_rgnend = rgnend
-      max_name = name
-    end
-  end
+if playstate & 1 == 0 then
+   cursor_pos = reaper.GetCursorPosition()
+else
+   cursor_pos = reaper.GetPlayPosition()
 end
 
---Change marker color
-if enum_index_of_max then
-  local r, g, b = 240, 40, 40
-  local color = reaper.ColorToNative(r, g, b) | 0x1000000
+local r, g, b = 240, 40, 40
+local color = reaper.ColorToNative(r, g, b) | 0x1000000
 
-  reaper.SetProjectMarkerByIndex2(0, enum_index_of_max, false, max_pos, max_rgnend, max_idx, max_name, color, 0)
-end
+reaper.AddProjectMarker2(0, false, cursor_pos, 0, "", -1, color)
+
+reaper.Undo_EndBlock("Insert red marker", -1)

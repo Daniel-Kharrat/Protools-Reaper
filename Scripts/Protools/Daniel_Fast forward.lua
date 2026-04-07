545b78a9fafec6742cce21d459c80bb566c23f5e
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-04-07 13:11:18 +0300

    Add commands to clear selections in script
    Added commands to clear time selection, razor edit areas, and item selections.

--- Scripts/Protools/Daniel_Fast forward.lua
+++ Scripts/Protools/Daniel_Fast forward.lua
@@ -1,5 +1,15 @@
 reaper.set_action_options(2) 
 
+--Time selection: Remove (unselect) time selection and loop points
+reaper.Main_OnCommand(40020,0)
+
+--Razor edit: Clear all areas
+reaper.Main_OnCommand(42406,0)
+
+--Item: Unselect (clear selection of) all items
+reaper.Main_OnCommand(40289,0)
+
+
 --View: Move edit cursor to play cursor
 reaper.Main_OnCommand(40434,0)
 --Transport: Stop

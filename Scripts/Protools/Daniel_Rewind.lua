70550a7c0b45e4017f19a1721026b9c905861cde
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-04-07 13:10:53 +0300

    Clear time selection and item selections in script
    Added commands to clear time selection, razor edit areas, and item selections.

--- Scripts/Protools/Daniel_Rewind.lua
+++ Scripts/Protools/Daniel_Rewind.lua
@@ -1,5 +1,15 @@
 reaper.set_action_options(2)
 
+--Time selection: Remove (unselect) time selection and loop points
+reaper.Main_OnCommand(40020,0)
+
+--Razor edit: Clear all areas
+reaper.Main_OnCommand(42406,0)
+
+--Item: Unselect (clear selection of) all items
+reaper.Main_OnCommand(40289,0)
+
+
 -- Get current arrange view position (start and end)
 local view_start, view_end = reaper.BR_GetArrangeView(0)
 

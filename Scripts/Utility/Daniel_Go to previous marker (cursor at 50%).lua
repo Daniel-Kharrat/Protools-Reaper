9c0848e7d5daa327e56f93a3e21fa2832fde241f
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-07-29 10:49:42 +0300

    Add script to navigate and manipulate markers and items

---
+++ Scripts/Utility/Daniel_Go to previous marker (cursor at 50%).lua
@@ -0,0 +1,14 @@
+--Time selection: Remove (unselect) time selection and loop points
+reaper.Main_OnCommand(40020,0)
+
+--Razor edit: Clear all areas
+reaper.Main_OnCommand(42406,0)
+
+--Item: Unselect (clear selection of) all items
+reaper.Main_OnCommand(40289,0)
+
+--Markers: Go to previous marker/project start
+reaper.Main_OnCommand(40172,0)
+
+--SWS: Horizontal scroll to put edit cursor at 50%
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_HSCROLL50"),0)

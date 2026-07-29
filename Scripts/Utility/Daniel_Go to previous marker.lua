71babf00832b4293460b6bf2d3ee56c1ddc5a886
Daniel-Kharrat <danielkharrat2@gmail.com>
2026-07-29 12:01:09 +0300

    Update Daniel_Go to previous marker.lua

--- Scripts/Utility/Daniel_Go to previous marker.lua
+++ Scripts/Utility/Daniel_Go to previous marker.lua
@@ -1,3 +1,7 @@
+--Get the state of "toggle horizontal scroll 50%" action
+local command_id = reaper.NamedCommandLookup("_RSb188ca992fb7f08eba2ac3633ab1972d2a6604bd")
+local state = reaper.GetToggleCommandState(command_id)
+
 --Time selection: Remove (unselect) time selection and loop points
 reaper.Main_OnCommand(40020,0)
 
@@ -9,3 +13,8 @@ reaper.Main_OnCommand(40289,0)
 
 --Markers: Go to previous marker/project start
 reaper.Main_OnCommand(40172,0)
+
+if state == 1 then
+  --SWS: Horizontal scroll to put edit cursor at 50%
+  reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_HSCROLL50"),0)
+end

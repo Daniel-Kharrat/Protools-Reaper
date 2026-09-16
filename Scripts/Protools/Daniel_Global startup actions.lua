0e999a11f5c8e0b58ebf1c9603905812d3725c41
Daniel Kharrat <danielkharrat2@gmail.com>
2026-09-16 13:38:23 +0300

    Add command to execute BirdBird_Global Sampler script

--- Scripts/Protools/Daniel_Global startup actions.lua
+++ Scripts/Protools/Daniel_Global startup actions.lua
@@ -11,3 +11,8 @@ reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS73872e52305a191a83f55f1811ac
 
 --Link Timeline and Edit Selection
 reaper.SetToggleCommandState(0, reaper.NamedCommandLookup("_RSdd4ba6262e05c57604ab621179a4552cf2bad49b"), 1)
+
+--Script: BirdBird_Global Sampler.lua
+pcall(function()
+    reaper.Main_OnCommand(reaper.NamedCommandLookup("_RSdbf64708ea8abea46b82a08cabc050148d65176c"), 0)
+end)

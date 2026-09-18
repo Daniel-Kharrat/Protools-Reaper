f134f0c77c449994a7f4310db0043ab41da20875
Daniel Kharrat <danielkharrat2@gmail.com>
2026-09-18 23:46:11 +0300

    Update Daniel_Global startup actions.lua

--- Scripts/Protools/Daniel_Global startup actions.lua
+++ Scripts/Protools/Daniel_Global startup actions.lua
@@ -9,5 +9,53 @@ reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS089967a15b5ae97c96687243d33f
 --Check for soloed tracks
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS73872e52305a191a83f55f1811ac45c380e98ff5"), 0)
 
+--------------------------------------------------
 --Link Timeline and Edit Selection
-reaper.SetToggleCommandState(0, reaper.NamedCommandLookup("_RSdd4ba6262e05c57604ab621179a4552cf2bad49b"), 1)
+--------------------------------------------------
+
+local personal_settings = reaper.GetResourcePath() .. "/Personal Settings"
+local toggle_file = personal_settings .. "/Toolbar_Toggles.ini"
+
+local command_id = reaper.NamedCommandLookup("_RSdd4ba6262e05c57604ab621179a4552cf2bad49b")
+
+local file = io.open(toggle_file, "r")
+
+if file then
+    for line in file:lines() do
+        local value = line:match("^Link_Timeline_and_Edit_Selection=(%d+)$")
+
+        if value == "1" then
+            reaper.SetToggleCommandState(0, command_id, tonumber(1))
+            reaper.RefreshToolbar2(0, command_id)
+            break
+        end
+    end
+
+    file:close()
+end
+
+--------------------------------------------------
+--Always Recording
+--------------------------------------------------
+
+local personal_settings = reaper.GetResourcePath() .. "/Personal Settings"
+local toggle_file = personal_settings .. "/Toolbar_Toggles.ini"
+
+local command_id = reaper.NamedCommandLookup("_RS1d334413686175f313d60578bea01a827ae4e954")
+
+local file = io.open(toggle_file, "r")
+
+if file then
+    for line in file:lines() do
+        local value = line:match("^Always_Recording=(%d+)$")
+
+        if value == "1" then
+            reaper.SetToggleCommandState(0, command_id, tonumber(1))
+            reaper.RefreshToolbar2(0, command_id)
+            reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS22184bfd14ba6fe71f7c982d8354aec893c6d2ba"), 0)
+            break
+        end
+    end
+
+    file:close()
+end

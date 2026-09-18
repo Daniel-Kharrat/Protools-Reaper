30942ae4dcac5fd0ead5cbbf48cf604998e92858
Daniel Kharrat <danielkharrat2@gmail.com>
2026-09-18 23:56:36 +0300

    Update Daniel_Always Recording (toolbar toggle).lua

--- Scripts/Utility/Daniel_Always Recording (toolbar toggle).lua
+++ Scripts/Utility/Daniel_Always Recording (toolbar toggle).lua
@@ -34,42 +34,6 @@ local function set_button_state(value)
   end
 end
 
-local personal_settings = reaper.GetResourcePath() .. "/Personal Settings"
-local toggle_file = personal_settings .. "/Toolbar_Toggles.ini"
-
-reaper.RecursiveCreateDirectory(personal_settings, 0)
-
-local function set_button_state(value)
-  local lines = {}
-  local found = false
-
-  local file = io.open(toggle_file, "r")
-
-  if file then
-    for line in file:lines() do
-      if line:match("^Always_Recording=") then
-        table.insert(lines, "Always_Recording=" .. tostring(value))
-        found = true
-      else
-        table.insert(lines, line)
-      end
-    end
-    file:close()
-  end
-
-  if not found then
-    table.insert(lines, "Always_Recording=" .. tostring(value))
-  end
-
-  file = io.open(toggle_file, "w")
-
-  if file then
-    file:write(table.concat(lines, "\n"))
-    file:write("\n")
-    file:close()
-  end
-end
-
 local _, _, _, command_id = reaper.get_action_context()
 local state = reaper.GetToggleCommandState(command_id)
 
